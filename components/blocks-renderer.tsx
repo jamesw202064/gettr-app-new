9c66852344eaeb28c204b51bfc0710106e94608d
xiaoge_engineer <jamesw202064@gmail.com>
2023-05-05 21:02:11 +0700

    add file

--- components/blocks-renderer.tsx
+++ components/blocks-renderer.tsx
@@ -1,56 +1,65 @@
-import React from "react";
-import type { Page } from "../tina/__generated__/types";
-import { Content } from "./blocks/content";
-import { Features } from "./blocks/features";
-import { Hero } from "./blocks/hero";
-import { Testimonial } from "./blocks/testimonial";
+import React from 'react';
+import type { Page } from '../tina/__generated__/types';
+import { Content } from './blocks/content';
+import { Features } from './blocks/features';
+import { Hero } from './blocks/hero';
+import { Testimonial } from './blocks/testimonial';
+import { Contact } from './blocks/page/contact';
+import { Leadership } from './blocks/page/leadership';
+import { Partner } from './blocks/page/partner';
 
-export const Blocks = (props: Omit<Page, "id" | "_sys" | "_values">) => {
+export const Blocks = (props: Omit<Page, 'id' | '_sys' | '_values'>) => {
   return (
     <>
       {props.blocks
         ? props.blocks.map(function (block, i) {
-          switch (block.__typename) {
-            case "PageBlocksContent":
-              return (
-                <div
-                  data-tinafield={`blocks.${i}`}
-                  key={i + block.__typename}
-                >
-                  <Content data={block} parentField={`blocks.${i}`} />
-                </div>
-              );
-            case "PageBlocksHero":
-              return (
-                <div
-                  data-tinafield={`blocks.${i}`}
-                  key={i + block.__typename}
-                >
-                  <Hero data={block} parentField={`blocks.${i}`} />
-                </div>
-              );
-            case "PageBlocksFeatures":
-              return (
-                <div
-                  data-tinafield={`blocks.${i}`}
-                  key={i + block.__typename}
-                >
-                  <Features data={block} parentField={`blocks.${i}`} />
-                </div>
-              );
-            case "PageBlocksTestimonial":
-              return (
-                <div
-                  data-tinafield={`blocks.${i}`}
-                  key={i + block.__typename}
-                >
-                  <Testimonial data={block} parentField={`blocks.${i}`} />
-                </div>
-              );
-            default:
-              return null;
-          }
-        })
+            switch (block.__typename) {
+              case 'PageBlocksContent':
+                return (
+                  <div data-tinafield={`blocks.${i}`} key={i + block.__typename}>
+                    <Content data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              case 'PageBlocksHero':
+                return (
+                  <div data-tinafield={`blocks.${i}`} key={i + block.__typename}>
+                    <Hero data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              case 'PageBlocksFeatures':
+                return (
+                  <div data-tinafield={`blocks.${i}`} key={i + block.__typename}>
+                    <Features data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              case 'PageBlocksTestimonial':
+                return (
+                  <div data-tinafield={`blocks.${i}`} key={i + block.__typename}>
+                    <Testimonial data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              case 'PagesBlocksContact':
+                return (
+                  <div data-tinafield={`blocks.${i}`} key={i + block.__typename}>
+                    <Contact data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              case 'PagesBlocksLeadership':
+                return (
+                  <div data-tinafield={`blocks.${i}`} key={i + block.__typename}>
+                    <Leadership data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              case 'PagesBlocksPartner':
+                return (
+                  <div data-tinafield={`blocks.${i}`} key={i + block.__typename}>
+                    <Partner data={block} parentField={`blocks.${i}`} />
+                  </div>
+                );
+              default:
+                return null;
+            }
+          })
         : null}
     </>
   );
--- components/blocks/hero.tsx
+++ components/blocks/hero.tsx
@@ -1,30 +1,27 @@
-import * as React from "react";
-import { Actions } from "../util/actions";
-import { Container } from "../util/container";
-import { Section } from "../util/section";
-import { useTheme } from "../layout";
-import { TinaMarkdown } from "tinacms/dist/rich-text";
-import type { TinaTemplate } from "tinacms";
+import * as React from 'react';
+import { Actions } from '../util/actions';
+import { Container } from '../util/container';
+import { Section } from '../util/section';
+import { useTheme } from '../layout/theme';
+import { TinaMarkdown } from 'tinacms/dist/rich-text';
+import type { TinaTemplate } from 'tinacms';
 
 export const Hero = ({ data, parentField }) => {
   const theme = useTheme();
   const headlineColorClasses = {
-    blue: "from-blue-400 to-blue-600",
-    teal: "from-teal-400 to-teal-600",
-    green: "from-green-400 to-green-600",
-    red: "from-red-400 to-red-600",
-    pink: "from-pink-400 to-pink-600",
-    purple: "from-purple-400 to-purple-600",
-    orange: "from-orange-300 to-orange-600",
-    yellow: "from-yellow-400 to-yellow-600",
+    blue: 'from-blue-400 to-blue-600',
+    teal: 'from-teal-400 to-teal-600',
+    green: 'from-green-400 to-green-600',
+    red: 'from-red-400 to-red-600',
+    pink: 'from-pink-400 to-pink-600',
+    purple: 'from-purple-400 to-purple-600',
+    orange: 'from-orange-300 to-orange-600',
+    yellow: 'from-yellow-400 to-yellow-600'
   };
 
   return (
     <Section color={data.color}>
-      <Container
-        size="large"
-        className="grid grid-cols-1 lg:grid-cols-5 gap-14 items-center justify-center"
-      >
+      <Container size="large" className="grid grid-cols-1 lg:grid-cols-5 gap-14 items-center justify-center">
         <div className="row-start-2 lg:row-start-1 lg:col-span-3 text-center lg:text-left">
           {data.tagline && (
             <h2
@@ -42,9 +39,7 @@ export const Hero = ({ data, parentField }) => {
             >
               <span
                 className={`bg-clip-text text-transparent bg-gradient-to-r  ${
-                  data.color === "primary"
-                    ? `from-white to-gray-100`
-                    : headlineColorClasses[theme.color]
+                  data.color === 'primary' ? `from-white to-gray-100` : headlineColorClasses[theme.color]
                 }`}
               >
                 {data.headline}
@@ -55,7 +50,7 @@ export const Hero = ({ data, parentField }) => {
             <div
               data-tinafield={`${parentField}.text`}
               className={`prose prose-lg mx-auto lg:mx-0 mb-10 ${
-                data.color === "primary" ? `prose-primary` : `dark:prose-dark`
+                data.color === 'primary' ? `prose-primary` : `dark:prose-dark`
               }`}
             >
               <TinaMarkdown content={data.text} />
@@ -93,99 +88,99 @@ export const Hero = ({ data, parentField }) => {
 };
 
 export const heroBlockSchema: TinaTemplate = {
-  name: "hero",
-  label: "Hero",
+  name: 'hero',
+  label: 'Hero',
   ui: {
-    previewSrc: "/blocks/hero.png",
+    previewSrc: '/blocks/hero.png',
     defaultItem: {
       tagline: "Here's some text above the other text",
-      headline: "This Big Text is Totally Awesome",
-      text: "Phasellus scelerisque, libero eu finibus rutrum, risus risus accumsan libero, nec molestie urna dui a leo.",
-    },
+      headline: 'This Big Text is Totally Awesome',
+      text: 'Phasellus scelerisque, libero eu finibus rutrum, risus risus accumsan libero, nec molestie urna dui a leo.'
+    }
   },
   fields: [
     {
-      type: "string",
-      label: "Tagline",
-      name: "tagline",
+      type: 'string',
+      label: 'Tagline',
+      name: 'tagline'
     },
     {
-      type: "string",
-      label: "Headline",
-      name: "headline",
+      type: 'string',
+      label: 'Headline',
+      name: 'headline'
     },
     {
-      label: "Text",
-      name: "text",
-      type: "rich-text",
+      label: 'Text',
+      name: 'text',
+      type: 'rich-text'
     },
     {
-      label: "Actions",
-      name: "actions",
-      type: "object",
+      label: 'Actions',
+      name: 'actions',
+      type: 'object',
       list: true,
       ui: {
         defaultItem: {
-          label: "Action Label",
-          type: "button",
+          label: 'Action Label',
+          type: 'button',
           icon: true,
-          link: "/",
+          link: '/'
         },
-        itemProps: (item) => ({ label: item.label }),
+        itemProps: (item) => ({ label: item.label })
       },
       fields: [
         {
-          label: "Label",
-          name: "label",
-          type: "string",
+          label: 'Label',
+          name: 'label',
+          type: 'string'
         },
         {
-          label: "Type",
-          name: "type",
-          type: "string",
+          label: 'Type',
+          name: 'type',
+          type: 'string',
           options: [
-            { label: "Button", value: "button" },
-            { label: "Link", value: "link" },
-          ],
+            { label: 'Button', value: 'button' },
+            { label: 'Link', value: 'link' }
+          ]
         },
         {
-          label: "Icon",
-          name: "icon",
-          type: "boolean",
+          label: 'Icon',
+          name: 'icon',
+          type: 'boolean'
         },
         {
-          label: "Link",
-          name: "link",
-          type: "string",
-        },
-      ],
+          label: 'Link',
+          name: 'link',
+          type: 'string'
+        }
+      ]
     },
     {
-      type: "object",
-      label: "Image",
-      name: "image",
+      type: 'object',
+      label: 'Image',
+      name: 'image',
       fields: [
         {
-          name: "src",
-          label: "Image Source",
-          type: "image",
+          name: 'src',
+          label: 'Image Source',
+          type: 'image'
         },
         {
-          name: "alt",
-          label: "Alt Text",
-          type: "string",
-        },
-      ],
+          name: 'alt',
+          label: 'Alt Text',
+          type: 'string'
+        }
+      ]
     },
     {
-      type: "string",
-      label: "Color",
-      name: "color",
+      type: 'string',
+      label: 'Color',
+      name: 'color',
       options: [
-        { label: "Default", value: "default" },
-        { label: "Tint", value: "tint" },
-        { label: "Primary", value: "primary" },
-      ],
-    },
-  ],
+        { label: 'Default', value: 'default' },
+        { label: 'Tint', value: 'tint' },
+        { label: 'Primary', value: 'primary' }
+      ]
+    }
+  ]
 };
